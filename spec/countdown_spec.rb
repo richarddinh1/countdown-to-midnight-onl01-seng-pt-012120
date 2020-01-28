a356ea3a0db54bfbd84451c2cdc8d5330d211ae3
richard dinh <richardndinh@gmail.com>
2020-01-28 17:53:42 +0000

    Done.

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -23,3 +23,22 @@ describe 'LOOPS' do
   end
 
 end
+
+def countdown(int)
+  while int > 0
+  puts "#{int} SECOND(S)!"
+  int -= 1
+ end
+ "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(int)
+  while int > 0
+  puts "#{int} SECOND(S)!"
+  sleep(1)
+  int -= 1 
+ end
+ "HAPPY NEW YEAR!"
+end
